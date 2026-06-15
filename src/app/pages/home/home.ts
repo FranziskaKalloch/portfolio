4d6ef463b47a52e7a3d82fa0dd0fd3dae0f343d7
Franziska Kalloch <franzy@MacBookAir.fritz.box>
2026-06-15 14:44:37 +0200

    build animation for logo icon

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Header } from '../../layout/header/header';
 import { Hero } from '../../sections/hero/hero';
+import { WhyMe } from '../../sections/why-me/why-me';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, WhyMe, Header],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
