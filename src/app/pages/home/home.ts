b6806bd9b59afd00461346272ee0638833ac9dd1
Franziska Kalloch <franzy@MacBookAir.fritz.box>
2026-06-16 11:48:12 +0200

    build skills component with style and html

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { Header } from '../../layout/header/header';
 import { Hero } from '../../sections/hero/hero';
+import { Skills } from '../../sections/skills/skills';
 import { WhyMe } from '../../sections/why-me/why-me';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, WhyMe, Header],
+  imports: [Hero, WhyMe, Header, Skills],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
