36c28902ee8aa03021d8b86f63b1026e6c85d50f
Franziska Kalloch <franzy@MacBookAir.fritz.box>
2026-06-16 17:45:02 +0200

    Create projects section with tabs and project card layout

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 import { Header } from '../../layout/header/header';
 import { Hero } from '../../sections/hero/hero';
+import { Projects } from '../../sections/projects/projects';
 import { Skills } from '../../sections/skills/skills';
 import { WhyMe } from '../../sections/why-me/why-me';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, WhyMe, Header, Skills],
+  imports: [Hero, WhyMe, Header, Skills, Projects],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- src/app/sections/projects/projects.ts
+++ src/app/sections/projects/projects.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {}
+export class Projects {
+  activeProject = 0;
+
+  projects = [];
+}
