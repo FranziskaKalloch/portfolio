388852e70a2ec5f1b7235639afede2f4fc419d55
Franziska Kalloch <franzy@MacBookAir.fritz.box>
2026-06-19 12:03:36 +0200

    feat: finish references section layout and hover effects

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { Header } from '../../layout/header/header';
 import { Hero } from '../../sections/hero/hero';
 import { Projects } from '../../sections/projects/projects';
+import { References } from '../../sections/references/references';
 import { Skills } from '../../sections/skills/skills';
 import { WhyMe } from '../../sections/why-me/why-me';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, WhyMe, Header, Skills, Projects],
+  imports: [Hero, WhyMe, Header, Skills, Projects, References],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
