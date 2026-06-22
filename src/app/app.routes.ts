1049f8da25a99451d7201ebc51ccf52957657fac
Franziska Kalloch <franzy@MacBookAir.fritz.box>
2026-06-22 14:26:14 +0200

    add legal notice content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { Home } from './pages/home/home';
 import { LegalNotice } from './pages/legal-notice/legal-notice';
 import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
+// import { WhyMe } from './sections/why-me/why-me';
+
 export const routes: Routes = [
   {
     path: '',
     component: Home,
   },
+  // { path: 'why-me', component: WhyMe },
+
   {
     path: 'legal-notice',
     component: LegalNotice,
