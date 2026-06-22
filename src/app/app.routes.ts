14f81609097e2aeeb3a28274e2ed100b6033d84b
Franziska Kalloch <franzy@MacBookAir.fritz.box>
2026-06-22 12:25:54 +0200

    add legal notice and privacy policy routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,20 @@ import { Routes } from '@angular/router';
 
 import { Header } from './layout/header/header';
 import { Home } from './pages/home/home';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'legal-notice',
+    component: LegalNotice,
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicy,
   },
 ];
